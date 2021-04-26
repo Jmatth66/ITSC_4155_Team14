7dbf4ea5b469cce70f72560b20ef04c3991542fd
Wyatt Barrier <wbarrie2@uncc.edu>
2021-04-26 13:20:03 -0400

    Email fix

--- js/checkout.js
+++ js/checkout.js
@@ -4,6 +4,9 @@ window.addEventListener('load', function () {
     StartUp();
 })
 
+var total = 0;
+var contents;
+
 function StartUp() {
     //checks browser support for local storage
     if ('localStorage' in window && window['localStorage'] == null) {
@@ -14,15 +17,18 @@ function StartUp() {
         //initialize a empty checkout table
         var cartList = "<tr><th>Item</th><th>Quantity</th><th>Price Per Unit</th><th>Sub-Total</th></tr>\n";
         var i = 0;
-		var total = 0;
 		var subTotal;
+		
+		total = 0;
+		contents = cartList;
 
         //takes the storage length and goes through each item grabbing the string associated with each key and deconstructs its data
         for (i = 0; i <= localStorage.length - 1; i++) {
-            productKey = localStorage.getItem(localStorage.key(i))
+            productKey = localStorage.getItem(localStorage.key(i));
             let temp = productKey.split(".");
 			subTotal = (temp[1] * temp[2]);
             cartList += "<tr><td>" + temp[0] + "</td>\n<td>" + temp[1] + "</td>\n<td>$" + temp[2] + "</td>\n<td>$" + subTotal + "</td>\n<td><input type=\"button\" class=\"delete\" value=\"Remove\" onclick=\"deleteItem(this)\"></td></tr>";
+			contents += "<tr><td>" + temp[0] + "</td>\n<td>" + temp[1] + "</td>\n<td>$" + temp[2] + "</td>\n<td>$" + subTotal;
 			total += subTotal;
         }
 
@@ -43,3 +49,26 @@ function ClearAll() {
     localStorage.clear();
     StartUp();
 }
+
+window.onload = function() 
+		{
+            document.getElementById('contact-form').addEventListener('submit', function(event) {
+                event.preventDefault();
+                //emailjs.sendForm('service_yob21g8', 'template_5rq5evc', this)
+				
+				emailjs.send("service_yob21g8","template_5rq5evc",
+				{
+					name: document.getElementById('name').value,
+					email: document.getElementById('email').value,
+					phone: document.getElementById('phone').value,
+					cost: total,
+					contents: contents,
+					comments: document.getElementById('comments').innerHTML,
+				})
+                    .then(function() {
+                        console.log('SUCCESS!');
+                    }, function(error) {
+                        console.log('FAILED...', error);
+                    });
+            });
+        }
